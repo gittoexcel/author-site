f57843f485e39e3e88ff2cc09102df1d921456c2
gittoexcel <b.rayane2501@gmail.com>
2025-03-25 14:13:32 +0100

    Add files via upload

---
+++ acceuil.js
@@ -0,0 +1,9 @@
+function toggleBioJett() {
+    document.getElementById("bioEgo").style.display = "none";
+    document.getElementById("bioJett").style.display = "block";
+}
+
+function toggleBioEgo() {
+    document.getElementById("bioJett").style.display = "none";
+    document.getElementById("bioEgo").style.display = "block";
+}
